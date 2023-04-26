ecacb0061faabb36a9b9b6408481dc1e40cfa31c
kobeobuah <kobeobuah@gmail.com>
2023-04-26 15:01:30 +0000

    added a header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Header />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+const Header = () => {
+    
+  
+    return (
+      <>
+          <header className={`header`}>
+              <div>
+                  <h1>Where in the world?</h1>
+              </div>
+  
+              <div className='moon-dark-mode'>
+                  <i className="fas fa-moon"> Dark Mode</i>
+              </div>
+          </header>
+      </>
+    )
+  }
+  
+  export default Header
