5b1cde187ba439e8595797cc389aff9d9a9b3a5c
kobeobuah <kobeobuah@gmail.com>
2023-04-27 09:28:23 +0000

    changed component name to countryDetails

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './index.css';
 import Header from './components/Header';
 import Countries from './components/Countries';
-import Country from './components/Country';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Countries />} />
-        <Route path="/countries/:name" element={<Country />} />
+        <Route path="/countries/:name" element={<CountryDetails />} />
       </Routes>
     </Router>
     </div>
