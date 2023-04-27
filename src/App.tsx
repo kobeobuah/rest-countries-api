8ca87c47b26cbb0236ed3094565a4b43649668ef
kobeobuah <kobeobuah@gmail.com>
2023-04-27 15:40:33 +0000

    working on theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { createContext, useState } from "react";
 import './index.css';
 import Header from './components/Header';
 import Countries from './components/Countries';
 import CountryDetails from './components/CountryDetails';
 
+interface ThemeContextProps {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextProps | null>(null);
+
 function App() {
+  const [theme, setTheme] = useState<string>("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "dark" ? "light" : "dark"));
+  };
+
   return (
-    
-    <div className="dark" >
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <div className="dark" id={theme}>
     <Router>
-      <Header />
+      <Header toggleTheme={toggleTheme}/>
       <Routes>
         <Route path="/" element={<Countries />} />
         <Route path="/countries/:name" element={<CountryDetails />} />
       </Routes>
     </Router>
     </div>
-    
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,22 @@
-const Header = () => {
-    
-  
+import { useContext } from "react";
+import { ThemeContext } from "../App";
+
+interface Props {
+    toggleTheme: () => void;
+  }
+
+  const Header: React.FC<Props> = ({ toggleTheme }: Props) => {
+    const themeContext = useContext(ThemeContext);
+    const theme = themeContext?.theme;
     return (
       <>
-          <header className={`header`}>
+          <header className={`header ${theme}`}>
               <div>
                   <h1>Where in the world?</h1>
               </div>
   
               <div className='moon-dark-mode'>
-                  <i className="fas fa-moon"> Dark Mode</i>
+                  <i className="fas fa-moon" onClick={() => toggleTheme()}> Dark Mode</i>
               </div>
           </header>
       </>
